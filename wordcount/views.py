8e609eedd0174c9981336a0e39510176181be231
matin nazaribaqa <matinnazaribaqa@gmail.com>
2020-02-20 21:10:14 +0330

    this is my first commit

---
+++ wordcount/views.py
@@ -0,0 +1,27 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import operator
+
+def home(request):
+  return render(request, "home.html" ) 
+
+def count(request):
+  fulltext = request.GET["fulltext"]
+
+  word_list = fulltext.split()
+  word_dictionary = {} 
+
+  for word in word_list:
+    if word in word_dictionary:
+      #increase
+      word_dictionary[word] += 1
+    else:
+      #add to the dictionary
+      word_dictionary[word] = 1
+
+  sorted_words = sorted(word_dictionary.items(), key=operator.itemgetter(1), reverse=True)
+
+  return render(request, "count.html",{"fulltext": fulltext,"count": len(word_list), "sorted_words": sorted_words})
+
+def aboutUs(request):
+  return render (request, "aboutUs.html")
